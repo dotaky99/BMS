cabd849176900292e857f720723ca099edc25834
wkddb <wkddbwls801>
2020-11-25 22:32:19 +0900

    YJ edit

--- Parse.py
+++ Parse.py
@@ -1,25 +1,25 @@
-# from EventLogParse import Save_Event
-# from LnkParse import Lnk_Parse
-# from BrowserParse import BrowserParser
-# from PretchParse import Prefetch_Parse
-# import NTFSParse
+from EventLogParse import Save_Event
+from LnkParse import Lnk_Parse
+from BrowserParse import BrowserParser
+from PretchParse import Prefetch_Parse
+from NTFSParse import MFT_Parser
 import os
 
 if __name__ == "__main__":
-    # print("[*] Parsing Event Log")
-    # Save_Event.Save_Event()
-    #
-    # print("[*] Parsing Lnk")
-    # Lnk_Parse.main()
-    #
-    # print("[*] Parsing Browser")
-    # BrowserParser.Browser_parser()
-    #
-    # print("[*] Parsing Prefetch")
-    # Prefetch_Parse.main()
+    print("[*] Parsing Event Log")
+    Save_Event.Save_Event()
 
-    # print("[*] Parsing MFT")
-    # MFT_Parser.parsing()
+    print("[*] Parsing Lnk")
+    Lnk_Parse.main()
 
-    print("[*] Parsing Registry")
-    os.system('python RegistryParse\REGParse.py COPY\REGHIVE\SYSTEM COPY\REGHIVE\SOFTWARE COPY\REGHIVE\SAM COPY\REGHIVE\\NTUSER.DAT COPY\REGHIVE\\USRCLASS.DAT')
+    print("[*] Parsing Browser")
+    BrowserParser.Browser_parser()
+
+    print("[*] Parsing Prefetch")
+    Prefetch_Parse.main()
+
+    print("[*] Parsing MFT")
+    MFT_Parser.parsing()
+
+    # print("[*] Parsing Registry")
+    # os.system('python RegistryParse\REGParse.py COPY\REGHIVE\SYSTEM COPY\REGHIVE\SOFTWARE COPY\REGHIVE\SAM COPY\REGHIVE\\NTUSER.DAT COPY\REGHIVE\\USRCLASS.DAT')
