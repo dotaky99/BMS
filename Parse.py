0da315203c7ee0c6650ce2dee4f7cfe4c93c7e78
dotaky99 <dotaky99@gmail.com>
2020-11-25 14:56:36 +0900

    TS edit

--- Parse.py
+++ Parse.py
@@ -6,20 +6,20 @@ from PretchParse import Prefetch_Parse
 import os
 
 if __name__ == "__main__":
-    print("[*] Parsing Event Log")
-    Save_Event.Save_Event()
+    # print("[*] Parsing Event Log")
+    # Save_Event.Save_Event()
 
     print("[*] Parsing Lnk")
     Lnk_Parse.main()
 
-    print("[*] Parsing Browser")
-    BrowserParser.Browser_parser()
+    # print("[*] Parsing Browser")
+    # BrowserParser.Browser_parser()
 
-    print("[*] Parsing Prefetch")
-    Prefetch_Parse.main()
+    # print("[*] Parsing Prefetch")
+    # Prefetch_Parse.main()
 
     # print("[*] Parsing MFT")
     # MFT_Parser.parsing()
 
-    print("[*] Parsing Registry")
-    os.system('python RegistryParse\REGParse.py REGHIVE\SYSTEM REGHIVE\SOFTWARE REGHIVE\SAM REGHIVE\82102.NTUSER.DAT REGHIVE\82102.USRCLASS.DAT')
+    # print("[*] Parsing Registry")
+    # os.system('python RegistryParse\REGParse.py REGHIVE\SYSTEM REGHIVE\SOFTWARE REGHIVE\SAM REGHIVE\82102.NTUSER.DAT REGHIVE\82102.USRCLASS.DAT')
