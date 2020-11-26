57b84b2e61fbeb921df83390dfb4ad229b317968
dotaky99 <dotaky99@gmail.com>
2020-11-26 11:12:03 +0900

    TS edit

--- RegistryParse/REGParse.py
+++ RegistryParse/REGParse.py
@@ -1,7 +1,6 @@
 import sys
 import time
 import os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from RegistryParse import decode
 import struct
 from RegistryParse.Registry import Registry
