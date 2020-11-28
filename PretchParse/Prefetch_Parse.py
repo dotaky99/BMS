bd11b2dc9dec1b497ba6da44360cd0965609741e
wkddb <wkddbwls801>
2020-11-28 23:47:40 +0900

    YJ edit

--- PretchParse/Prefetch_Parse.py
+++ PretchParse/Prefetch_Parse.py
@@ -320,7 +320,10 @@ class DecompressWin10(object):
 def main():
     for i in file_list:
         if i.endswith(".pf"):
-            Prefetch(path_dir + i)
+            try:
+                Prefetch(path_dir + i)
+            except:
+                pass
         else:
             pass
 
